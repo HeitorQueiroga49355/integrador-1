0348597a4a4522acf3317adb2f12ce9781c9403e
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-11-18 14:54:45 -0300

    feat: corrigindo a url, adcionado static ao link css, extendendo base para templates de pesquisador editais

--- projeto-flow/pesquisador/urls.py
+++ projeto-flow/pesquisador/urls.py
@@ -1,13 +1,11 @@
-from .views import pesquisador_editais, pesquisador_projetos
-from . import views
+from .views import pesquisador_editais, pesquisador_projetos, pesquisador_projetos_detalhes, base
+# from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('base/', views.base, name='base'),
+    path('base/', base, name='base'),
 
-    path('', pesquisador_editais, name='pesquisador_editais'),
-    path('projetos/', pesquisador_projetos, name='pesquisador_projetos'),
-
-    path('meus-projetos/', views.pesquisador_projetos, name='pesquisador-projetos'),
-    path('meus-projetos-detalhes/', views.pesquisador_projetos_detalhes, name='pesquisador-projetos-detalhes')
+    path('', pesquisador_editais, name='pesquisador-editais'),
+    path('projetos/', pesquisador_projetos, name='pesquisador-projetos'),
+    path('projetos/detalhes/', pesquisador_projetos_detalhes, name='pesquisador-projetos-detalhes')
 ]
