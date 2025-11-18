3a6801ed74d7c20eb0d5da69682252f4a5f4f64d
Heitor Queiroga Duarte <heitorqueiroga1@hotmail.com>
2025-11-18 20:30:30 -0300

    adding register route

--- projeto-flow/user/views.py
+++ projeto-flow/user/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def login(request):
   return render(request, './login/index.html')
+
+def register(request):
+    return render(request, './register/index.html')
