5c0252262058db405a424ec5cba2b8ad47ef3c26
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2025-12-01 21:36:24 -0300

    fix: adcionando verbose name nos models de proposals

--- projeto-flow/proposals/models.py
+++ projeto-flow/proposals/models.py
@@ -4,10 +4,10 @@ from base.models import Base
 
 class Proposal(Base):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    departament = models.CharField(max_length=100)
-    proprsal_file = models.FileField(upload_to='institutions/', null=True, blank=True)
+    title = models.CharField(max_length=255, verbose_name='Título')
+    description = models.TextField(verbose_name='Descrição')
+    departament = models.CharField(max_length=100, verbose_name='Departamento')
+    proprsal_file = models.FileField(upload_to='institutions/', null=True, blank=True, verbose_name='Arquivo')
 
     def __str__(self):
         return self.title
@@ -15,10 +15,10 @@ class Proposal(Base):
 class RectifyProposal(Base):
     id = models.AutoField(primary_key=True)
     proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE, related_name='rectifications')
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    departament = models.CharField(max_length=100)
-    rectify_file = models.FileField(upload_to='institutions/', null=True, blank=True)
+    title = models.CharField(max_length=255, verbose_name='Título')
+    description = models.TextField(verbose_name='Descrição')
+    departament = models.CharField(max_length=100, verbose_name='Departamento')
+    rectify_file = models.FileField(upload_to='institutions/', null=True, blank=True, verbose_name='Arquivo')
 
     def __str__(self):
         return self.title
