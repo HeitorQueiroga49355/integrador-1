0fc931135cda69eea8744661f3e5423760ab0bea
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-01-21 17:02:13 -0300

    teste: teste para listar profiles cadastrados

--- projeto-flow/user/urls.py
+++ projeto-flow/user/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
   path('login/', views.login, name='login'),
   path('register/', views.register, name='register'),
+
+  path('list/profile/', views.ProfileListView.as_view(), name='profile-list'),
 ] 
--- projeto-flow/user/views.py
+++ projeto-flow/user/views.py
@@ -1,4 +1,7 @@
+from pyexpat import model
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView
+from .models import Profile
 
 # Create your views here.
 
@@ -7,3 +10,8 @@ def login(request):
 
 def register(request):
     return render(request, './register/index.html')
+
+class ProfileListView(ListView):
+  model = Profile
+  queryset = Profile.objects.all()
+  template_name = 'user/user_list.html'
