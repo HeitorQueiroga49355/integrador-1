f46d7c7612f51ca0fa2378f15610a50d01acca44
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-01-22 17:06:24 -0300

    feat: listar projetos criados pelo avaliador

--- projeto-flow/pesquisador/urls.py
+++ projeto-flow/pesquisador/urls.py
@@ -1,12 +1,14 @@
-from .views import pesquisador_editais, pesquisador_projetos, pesquisador_projetos_detalhes, base, pesquisador_adicionar_projeto, pesquisador_editar_projeto
-# from . import views
+from .views import PesquisadorEditaisView, ProjectListView, pesquisador_projetos, pesquisador_projetos_detalhes, base, pesquisador_adicionar_projeto, pesquisador_editar_projeto
+from . import views
 from django.urls import path
 
 urlpatterns = [
     path('base/', base, name='base'),
 
-    path('', pesquisador_editais, name='pesquisador-editais'),
-    path('projetos/', pesquisador_projetos, name='pesquisador-projetos'),
+    # path('', pesquisador_editais, name='pesquisador-editais'),
+    path('', views.PesquisadorEditaisView.as_view(), name='pesquisador-editais'),
+    # path('projetos/', pesquisador_projetos, name='pesquisador-projetos'),
+    path('projetos/', views.ProjectListView.as_view(), name='pesquisador-projetos'),
     path('projetos/adicionar/', pesquisador_adicionar_projeto, name='pesquisador-adicionar-projeto'),
     path('projetos/detalhes/', pesquisador_projetos_detalhes, name='pesquisador-projetos-detalhes'), 
     path('projetos/detalhes/editar/', pesquisador_editar_projeto, name='pesquisador-projetos-detalhes-editar'), 
