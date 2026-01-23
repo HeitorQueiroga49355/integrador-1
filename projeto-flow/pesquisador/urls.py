185f3152081ff88f3108fc279b39509d5af4e5c5
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-01-23 15:13:38 -0300

    feat: cbv para create projeto

--- projeto-flow/pesquisador/urls.py
+++ projeto-flow/pesquisador/urls.py
@@ -1,15 +1,14 @@
-from .views import PesquisadorEditaisView, ProjectListView, ProjectCreateView, pesquisador_projetos, pesquisador_projetos_detalhes, base, pesquisador_adicionar_projeto, pesquisador_editar_projeto
-from . import views
+from .views import PesquisadorEditaisView, ProjectListView, ProjectCreateView, pesquisador_projetos_detalhes, base, pesquisador_editar_projeto
 from django.urls import path
 
 urlpatterns = [
     path('base/', base, name='base'),
 
     # path('', pesquisador_editais, name='pesquisador-editais'),
-    path('', views.PesquisadorEditaisView.as_view(), name='pesquisador-editais'),
+    path('', PesquisadorEditaisView.as_view(), name='pesquisador-editais'),
     # path('projetos/', pesquisador_projetos, name='pesquisador-projetos'),
-    path('projetos/', views.ProjectListView.as_view(), name='pesquisador-projetos'),
-    path('projetos/adicionar/', views.ProjectCreateView.as_view(), name='pesquisador-adicionar-projeto'),
+    path('projetos/', ProjectListView.as_view(), name='pesquisador-projetos'),
+    path('projetos/adicionar/', ProjectCreateView.as_view(), name='pesquisador-adicionar-projeto'),
     path('projetos/detalhes/', pesquisador_projetos_detalhes, name='pesquisador-projetos-detalhes'), 
     path('projetos/detalhes/editar/', pesquisador_editar_projeto, name='pesquisador-projetos-detalhes-editar'), 
 ]
--- projeto-flow/pesquisador/views.py
+++ projeto-flow/pesquisador/views.py
@@ -10,9 +10,6 @@ from django.urls import reverse_lazy
 
 class PesquisadorEditaisView(TemplateView):
   template_name = 'pesquisador/editais.html'
-
-def pesquisador_projetos(request):
-    return render(request, 'pesquisador/pesquisador_meus_projetos_tabela.html')
     
 def base(request):
   return render(request, './base/base.html')
@@ -25,11 +22,18 @@ class ProjectListView(ListView):
   template_name = 'pesquisador/pesquisador_meus_projetos_tabela.html'
   context_object_name = 'projects'
 
+
 def pesquisador_projetos_detalhes(request):
   return render(request, 'pesquisador/pesquisador_meus_projetos_detalhes.html')
 
-def pesquisador_adicionar_projeto(request):
-  return render(request, 'pesquisador/adicionar_projeto.html')
+# def pesquisador_adicionar_projeto(request):
+#   return render(request, 'pesquisador/adicionar_projeto.html')
+
+class ProjectCreateView(CreateView):
+  model = Project
+  form_class = CreateProjectForm
+  template_name = 'pesquisador/adicionar_projeto.html'
+  success_url = reverse_lazy('pesquisador-projetos')
 
 def pesquisador_editar_projeto(request):
   return render(request, 'pesquisador/editar_projeto.html')
