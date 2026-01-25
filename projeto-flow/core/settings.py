5e9f0c27bf31067f39038ac881d8cfc3b9c1afd9
Tallys Cesar <tallycesardev@gmail.com>
2026-01-25 13:43:33 -0300

    migrate `sqlite` to `postgres` with `docker`

--- projeto-flow/core/settings.py
+++ projeto-flow/core/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'integrador-1',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'integrador_db',
+        'PORT': 5432,
     }
 }
 
