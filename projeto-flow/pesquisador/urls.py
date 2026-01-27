3c9421fdadf00ed3a89065a5041b033ece4f66dc
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-01-27 08:59:39 -0300

    feat: listando submissions em pesquisador meus projetos

--- projeto-flow/pesquisador/urls.py
+++ projeto-flow/pesquisador/urls.py
@@ -1,12 +1,12 @@
-from .views import ProposalListVew, pesquisador_projetos, pesquisador_projetos_detalhes, base, pesquisador_adicionar_projeto, pesquisador_editar_projeto
+from .views import ProposalListView, SubmissionListView, pesquisador_projetos_detalhes, base, pesquisador_adicionar_projeto, pesquisador_editar_projeto
 from . import views
 from django.urls import path
 
 urlpatterns = [
     path('base/', base, name='base'),
 
-    path('', ProposalListVew.as_view(), name='pesquisador-editais'),
-    path('projetos/', pesquisador_projetos, name='pesquisador-projetos'),
+    path('', ProposalListView.as_view(), name='pesquisador-editais'),
+    path('projetos/', SubmissionListView.as_view(), name='pesquisador-projetos'),
     path('projetos/adicionar/', pesquisador_adicionar_projeto, name='pesquisador-adicionar-projeto'),
     path('projetos/detalhes/', pesquisador_projetos_detalhes, name='pesquisador-projetos-detalhes'), 
     path('projetos/detalhes/editar/', pesquisador_editar_projeto, name='pesquisador-projetos-detalhes-editar'), 
--- projeto-flow/pesquisador/views.py
+++ projeto-flow/pesquisador/views.py
@@ -1,26 +1,28 @@
 from pyexpat import model
 from tempfile import template
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from proposals.models import Proposal
+from submission.models import Submission
 
 # Create your views here.
 # def pesquisador_editais(request):
 #   return render(request, 'pesquisador/editais.html')
+def base(request):
+  return render(request, './base/base.html')
 
-class ProposalListVew(ListView):
+class ProposalListView(ListView):
   model = Proposal
   template_name = 'pesquisador/editais.html'
   context_object_name = 'proposals'
 
-def pesquisador_projetos(request):
-    return render(request, 'pesquisador/pesquisador_meus_projetos_tabela.html')
-    
-def base(request):
-  return render(request, './base/base.html')
+# def pesquisador_projetos(request):
+#   return render(request, 'pesquisador/pesquisador_meus_projetos_tabela.html')
 
-def pesquisador_projetos(request):
-  return render(request, 'pesquisador/pesquisador_meus_projetos_tabela.html')
+class SubmissionListView(ListView):
+  model = Submission
+  template_name = 'pesquisador/pesquisador_meus_projetos_tabela.html'
+  context_object_name = 'submissions'
 
 def pesquisador_projetos_detalhes(request):
   return render(request, 'pesquisador/pesquisador_meus_projetos_detalhes.html')
